c10a7a5e52b06218a89f916af10d9830386f7386
Dan Mayer <dan@mayerdan.com>
2012-10-02 00:13:43 -0400

    adding some repo logic

--- server_responder.rb
+++ server_responder.rb
@@ -3,6 +3,7 @@ require 'json'
 set :public_folder, File.dirname(__FILE__) + '/public'
 
 tmp_file = "tmp/last_request.txt"
+local_repos = ENV['LOCAL_REPOS'] || "/opt/bitnami/apps/"
 
 get '/' do
   @results = `churn`
@@ -13,5 +14,15 @@ end
 post '/' do
   @push = params
   File.open(tmp_file, 'w') {|f| f.write(@push.to_json) }
+  repo_url = params['payload']['repository']['url'] rescue nil
+  repo_name = params['payload']['repository']['name'] rescue nil
+  if repo_url && repo_name
+    repo_location = "#{local_repos}#{local_repos}"
+    if File.exists?(repo_location)
+      `cd #{repo_location}; git pull`
+    else
+      `cd #{local_repos}; git clone #{repo_url}`
+    end
+  end
   erb :index_push
 end
